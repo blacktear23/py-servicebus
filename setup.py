a4433adb399c4c5a39436566d6c8177bf0551175
Rain Lee <blacktear23@gmail.com>
2012-12-02 15:55:10 +0800

    Add more documents

--- setup.py
+++ setup.py
@@ -1,16 +1,20 @@
 from distutils.core import setup
 
-long_description = """A message bus framework for transfer messages between servers.
-System is using RabbitMQ as a message broker.
+long_description = """Provide a basic RPC and Message process framework based on RabbitMQ message server.
+
+Features:
+      1. Auto reconnect when network is down
+      2. Support SSL and dynamic token validation in message transfer
+      3. Use multi-path to auto switch RabbitMQ server when major RabbitMQ server is down
 """
 
 setup(name='servicebus',
       version='1.0',
       author='Rain Li',
       author_email='blacktear23@gmail.com',
-      url='https://github.com/blacktear23/servicebus',
-      download_url='https://github.com/blacktear23/servicebus',
-      description='Message Bus use for message transfer',
+      url='https://github.com/blacktear23/py-servicebus',
+      download_url='https://github.com/blacktear23/py-servicebus/downloads',
+      description='Provide a basic RPC and Message process framework based on RabbitMQ message server.',
       long_description=long_description,
       packages=['servicebus', 'servicebus.pika'],
       license='',
@@ -18,6 +22,7 @@ setup(name='servicebus',
                    'Intended Audience :: Developers',
                    'Operating System :: OS Independent',
                    'Programming Language :: Python :: 2',
+                   'License :: OSI Approved :: BSD License',
                    'Topic :: Message Bus',
                    'Topic :: Software Development :: Libraries',
                   ],
