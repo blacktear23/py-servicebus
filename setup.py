424e0b0eb8968ca0f9f5323153f1f7e0f35d5f11
yulai.li <yulai.li@speedycloud.cn>
2017-09-06 15:50:26 +0800

    Update for pip upload

--- setup.py
+++ setup.py
@@ -27,8 +27,6 @@ setup(
                  'Programming Language :: Python :: 2.7',
                  'Programming Language :: Python :: 3',
                  'Programming Language :: Python :: Implementation :: CPython',
-                 'Topic :: Message Bus',
                  'Topic :: Software Development :: Libraries',
-                 'Topic :: Software Development :: Libraries :: Python Modules',
                  ]
 )
