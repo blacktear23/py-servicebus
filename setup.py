12c5233209af115ec21ca041ae704fdf2401c0c2
yulai.li <blacktear23@gmail.com>
2018-09-10 17:03:16 +0800

    Update version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ Features:
 
 setup(
     name='py-servicebus',
-    version='2.7',
+    version='2.8',
     author='Rain Li',
     author_email='blacktear23@gmail.com',
     url='https://github.com/blacktear23/py-servicebus',
