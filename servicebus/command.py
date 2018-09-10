651a9b8764591bfe78ebde67f5494763c0b2916d
yulai.li <blacktear23@gmail.com>
2018-09-10 12:28:35 +0800

    Update cmd function for return str at python 3

--- servicebus/command.py
+++ servicebus/command.py
@@ -11,7 +11,8 @@ def cmd(cmd):
     process = None
     try:
         process = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, close_fds=True)
-        return process.communicate()
+        out, err = process.communicate()
+        return out.decode('utf-8'), err.decode('utf-8')
     finally:
         if process is not None:
             process.stdout.close()
